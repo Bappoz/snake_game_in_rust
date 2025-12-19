df91c0404d20612c2ea11fbd864f03b528bffb77
Bappoz <landradezanetti@gmail.com>
2025-12-18 23:24:26 -0300

    Created a snake game in Rust for learning to code in Rust

---
+++ src/draw.rs
@@ -0,0 +1,30 @@
+use piston_window::*;
+
+const BLOCK_SIZE: f64 = 25.0;
+
+pub fn to_coord(game_coord: i32) -> f64 {
+    (game_coord as f64) * BLOCK_SIZE
+}
+
+pub fn to_coord_u32(game_coord: i32) -> u32 {
+    to_coord(game_coord) as u32
+}
+
+pub fn draw_block<G: graphics::Graphics>(color: [f32; 4], x: i32, y: i32, con: &graphics::Context, g: &mut G){
+    let gui_x = to_coord(x);
+    let gui_y = to_coord(y);
+    
+    graphics::rectangle(color, [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE], con.transform, g);
+}
+
+pub fn draw_rectangle<G: graphics::Graphics>(color: [f32; 4], x: i32, y: i32, width: i32, height: i32, con: &graphics::Context, g: &mut G){
+    let x = to_coord(x);
+    let y = to_coord(y);
+
+    graphics::rectangle(
+        color, 
+        [x, y, BLOCK_SIZE * (width as f64), BLOCK_SIZE * (height as f64)],
+        con.transform,
+        g,
+    );
+}
